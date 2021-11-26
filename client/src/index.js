538c68dbd750468799149299aeb63b603e034844
Suraj Shende <surajshende247@gmail.com>
2021-11-26 16:58:28 +0530

    add: bootstrap add

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Bootstrap files
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
