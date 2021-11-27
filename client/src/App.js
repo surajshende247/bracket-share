fe7fdb7c566b29f1db871a4d236b495fa8a558db
Suraj Shende <surajshende247@gmail.com>
2021-11-27 14:59:50 +0530

    add: router for Home and PairProgramming

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { BrowserRouter,Switch,Route,Redirect } from 'react-router-dom';
+
+import Home from './Home/Home';
+import PairProgramming from './PairProgramming/PairProgramming';
 
 function App() {
   return (
-    <div className="App">
-       
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/pairprogramming" component={PairProgramming} />
+        <Route exact path="/home" component={Home} />
+        <Redirect from="*" to="/home" />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
