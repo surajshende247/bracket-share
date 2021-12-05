1cc62582b50f3ca1703c5253ec16f81c44555e8c
Suraj Shende <surajshende247@gmail.com>
2021-12-05 21:14:39 +0530

    refactor: logo, basic colors, fontweights and chat background

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -5,6 +5,7 @@ import './Home.css';
 
 import imgBoy from './../../assets/img/img-boy.png';
 import imgGirl from './../../assets/img/img-girl.png';
+import logo from './../../assets/img/logo.svg';
 
 function Home() {
     let navigate = useNavigate();
@@ -28,8 +29,20 @@ function Home() {
     }
 
     return (
-        <div>
-            <div className="row">
+        <div className="home-container">
+            <div className="p-2">
+                                   
+                <h1 className="text-center"><img src={logo} alt="logo" className="img-logo"/>Bracket Share</h1>
+                            
+                <h5 className="text-center sub-heading">
+                    A <span className="text-success fw-bold">Realtime</span>, {" "}
+                    <span className="text-primary fw-bold">Pair Programming</span>{" "} 
+                    platform for live code discussion, technical interview with {" "}
+                    <span className="text-danger fw-bold">Chat feature</span>.
+                </h5>
+            </div>
+
+            <div className="row mt-3 mb-3">
                 <div className="col-md-8 d-md-flex flex-row justify-content-evenly">
                     <div className="card-room-entry">
                         <h2 className="text-center">Join Room</h2>
@@ -49,7 +62,7 @@ function Home() {
                             value={roomData.roomCode}
                             onChange={(e) => setRoomData({...roomData, roomCode: e.target.value})}
                             required/>
-                            <button type="submit" className="btn btn-warning w-100 mt-3">Join Room</button>
+                            <button type="submit" className="btn btn-warning w-100 mt-3 fw-bold">Join Room</button>
                         </form>
                     </div>
                 
@@ -63,22 +76,29 @@ function Home() {
                             value={roomData.userName}
                             onChange={(e) => setRoomData({...roomData, userName: e.target.value})}
                             required/>
-                            <button type="submit" className="btn btn-warning w-100 mt-3">Create Room</button>
+                            <button type="submit" className="btn btn-warning w-100 mt-3 fw-bold">Create Room</button>
                         </form>
                     </div>
                    
                     
                 </div>
                 <div className="col-md-4">
-                    <h2>Bracket Share Information</h2>
+                    <h2>What is Bracket Share ?</h2>
+                    <p></p>
+                    <h3>✅Feature-1</h3>
+                    <h3>✅Feature-2</h3>
+                    <h3>✅Feature-3</h3>
+                    <h3>✅Feature-4</h3>
+                    <h3>✅Feature-5</h3>
                 </div>
             </div>
-            <div className="d-flex flex-row d-flex justify-content-around">
-                <a href="https://github.com/surajshende247/bracket-share" target="_blank" className="btn btn-lg btn-dark  mt-3"> View Source Code on Github <i className="fa fa-github"></i></a>
-                <a href="https://github.com/surajshende247/bracket-share/issues/new" target="_blank" className="btn btn-lg btn-danger  mt-3"> Report Bug <i className="fa fa-bug"></i></a>
-            </div>
+            
             <footer className="footer">
-                <div className="container text-center">
+            <div className="d-flex flex-row d-flex justify-content-end bg-light p-2">
+                <a href="https://github.com/surajshende247/bracket-share" target="_blank" className="btn btn-lg btn-dark me-3"> View Source Code on Github <i className="fa fa-github"></i></a>
+                <a href="https://github.com/surajshende247/bracket-share/issues/new" target="_blank" className="btn btn-lg btn-danger me-3"> Report Bug <i className="fa fa-bug"></i></a>
+            </div>
+                <div className="footer-text container text-center">
                     Made With ❤️ by <a href="https://github.com/surajshende247">Suraj Shende</a> 
                 </div>
             </footer>
